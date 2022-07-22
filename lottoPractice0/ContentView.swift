b1bcedaa6e322f31938ad825945c728da17d5ca5
Wongi <hpurs95@gmail.com>
2022-07-23 00:15:52 +0900

    Initial Commit

---
+++ lottoPractice0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lottoPractice0
+//
+//  Created by MacBook Air on 2022/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lottoPractice0/lottoPractice0App.swift
@@ -0,0 +1,17 @@
+//
+//  lottoPractice0App.swift
+//  lottoPractice0
+//
+//  Created by MacBook Air on 2022/07/23.
+//
+
+import SwiftUI
+
+@main
+struct lottoPractice0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
